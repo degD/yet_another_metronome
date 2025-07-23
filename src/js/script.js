2d680bb1afe02b3164f40be458ffd58e0396f5c3
degD <den.ege.der@gmail.com>
2025-07-23 16:22:37 +0300

    add dark mode toggle switch

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,13 @@
 
 // Constants about the metronome
-const BEAT_ACTIVE_COLOR   = "#f4581a"
-const BEAT_DEACTIVE_COLOR = "#494949ff"
 const SECONDS_IN_MINUTE   = 60
 const WINDOW_SECONDS      = 0.1
 const MAX_NUM_OF_BEATS    = 20
 const MAX_BPM             = 600
+const DEACTIVE_DARK       = "#252A34"
+const DEACTIVE_LIGHT      = "#EAEAEA"
+const ACTIVE_DARK         = "#0e72a0ff"
+const ACTIVE_LIGHT        = "#08D9D6"
 
 // Initialize Audio Context
 const audioContext  = new window.AudioContext();
@@ -95,10 +97,14 @@ function stopPlayingBeats() {
 function updateVisualActiveBeat(currentBeat) {
   const beatBoxes = document.querySelectorAll(".beat-box");
   beatBoxes.forEach((beatBox, i) => {
-    if (i == currentBeat)
-      beatBox["style"]["background"] = BEAT_ACTIVE_COLOR;
-    else
-      beatBox["style"]["background"] = BEAT_DEACTIVE_COLOR;
+    if (i == currentBeat) {
+      beatBox.classList.add("active-background");
+      beatBox.classList.remove("deactive-background");
+    }
+    else {
+      beatBox.classList.add("deactive-background");
+      beatBox.classList.remove("active-background");    
+    }
   })
 }
 
@@ -121,14 +127,20 @@ function toggleButtonVisual() {
   const playSvgIcon = document.getElementById("play-svg");
   const pauseSvgIcon = document.getElementById("pause-svg");
   if (isPlaying) {
+    metronomeButton.classList.add("deactive-background");
+    metronomeButton.classList.remove("active-background");    
     playSvgIcon["style"]["display"] = "none";
     pauseSvgIcon["style"]["display"] = "block";
-    pauseSvgIcon["style"]["fill"] = BEAT_ACTIVE_COLOR;
+    pauseSvgIcon.classList.add("active-fill");
+    pauseSvgIcon.classList.remove("deactive-fill");
   }
   else {
+    metronomeButton.classList.add("active-background");
+    metronomeButton.classList.remove("deactive-background");
     pauseSvgIcon["style"]["display"] = "none";
     playSvgIcon["style"]["display"] = "block";
-    playSvgIcon["style"]["fill"] = BEAT_ACTIVE_COLOR;
+    playSvgIcon.classList.add("deactive-fill");
+    playSvgIcon.classList.remove("active-fill");     
   }
 }
 
@@ -141,11 +153,22 @@ function isPositiveInteger(n) {
   return Number.isInteger(n) && n > 0;
 }
 
+/**
+ * Set CSS color variables by Javascript
+ * @param {string} activeColor Color string for active color
+ * @param {string} deactiveColor Color string for deactive color
+ */
+function setCssColors(activeColor, deactiveColor) {
+  const root = document.querySelector(':root');
+  root.style.setProperty('--active-color', activeColor);
+  root.style.setProperty('--deactive-color', deactiveColor);
+}
+
 // Change number of beat boxes displayed when input value changed
 document.getElementById("number-of-beats").addEventListener("input", evt => {
   let newNumberOfBeats = Number(evt.target.value);
   newNumberOfBeats = isPositiveInteger(newNumberOfBeats) ? newNumberOfBeats : 1;
-  newNumberOfBeats = min(MAX_NUM_OF_BEATS, newNumberOfBeats);
+  newNumberOfBeats = Math.min(MAX_NUM_OF_BEATS, newNumberOfBeats);
   updateVisualBeatNumber(newNumberOfBeats);
 });
 
@@ -158,8 +181,8 @@ document.getElementById("start-metronome-button").addEventListener("click", evt 
   let beatsPerMinute = Number(beatsPerMinuteInput.value);
   numberOfBeats = isPositiveInteger(numberOfBeats) ? numberOfBeats : 1;
   beatsPerMinute = isPositiveInteger(beatsPerMinute) ? beatsPerMinute : 1;
-  numberOfBeats = min(MAX_NUM_OF_BEATS, numberOfBeats);
-  beatsPerMinute = min(MAX_BPM, beatsPerMinute);
+  numberOfBeats = Math.min(MAX_NUM_OF_BEATS, numberOfBeats);
+  beatsPerMinute = Math.min(MAX_BPM, beatsPerMinute);
 
   if (isPlaying) {
     stopPlayingBeats();
@@ -175,19 +198,43 @@ document.getElementById("start-metronome-button").addEventListener("click", evt 
   }
 });
 
+// Get saved theme from localStorage (default: light)
+// Auto-check if dark mode
+document.addEventListener("DOMContentLoaded", () => {
+  const themeSwitch = document.getElementById("theme-switch-input");
+  const html = document.documentElement;
+  const savedTheme = localStorage.getItem("theme") || "light";
+  html.setAttribute("data-theme", savedTheme);
+  themeSwitch.checked = savedTheme === "dark";
+  if (savedTheme === "dark")
+    setCssColors(ACTIVE_DARK, DEACTIVE_DARK);
+  else
+    setCssColors(ACTIVE_LIGHT, DEACTIVE_LIGHT);
+  
+  // Update theme and save preference on switch
+  themeSwitch.addEventListener("change", (event) => {
+    const newTheme = event.target.checked ? "dark" : "light";
+    html.setAttribute("data-theme", newTheme);
+    localStorage.setItem("theme", newTheme);
+    if (newTheme === "dark")
+      setCssColors(ACTIVE_DARK, DEACTIVE_DARK);
+    else
+      setCssColors(ACTIVE_LIGHT, DEACTIVE_LIGHT);
+  });
+});
+
 // Set initial input values
 document.getElementById("number-of-beats").value = 4;
 document.getElementById("beats-per-minute").value = 60;
 
 // Set initial button icon
 document.getElementById("play-svg")["style"]["display"] = "block";
-document.getElementById("play-svg")["style"]["fill"] = BEAT_ACTIVE_COLOR;
+document.getElementById("play-svg").classList.add("deactive-fill");
 
 
 
 
-// Better theme/UI
-// Alternative theme toggle switch
-// pico css
+
+// Alternative theme toggle switch --- edge cases
 // Logo design
 // show statusbar android
